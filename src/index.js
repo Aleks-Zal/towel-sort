90fc1b725a3d9a37c5473689cff262717284ff8c
Aleksandr Zalutskiy <simplesash@gmail.com>
2021-02-16 17:09:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix && matrix != 0) {
+    let newMatrix = [];
+
+    matrix.map((item, index) => {
+      if (index % 2 != 0) {
+        newMatrix.push(item.reverse())
+      } else {
+        newMatrix.push(item);
+      }
+    });
+    if (newMatrix.join(',').split(',').map(item => +item) === [0]) {
+      return [];
+    }
+    return newMatrix.join(',').split(',').map(item => +item);
+  } else {
+    return [];
+  }
+
 }
